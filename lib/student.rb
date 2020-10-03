ece78f5124ecbb3fbb7a1bd3a5b2980df9ca466d
Sara A <calisaraalhindi@gmail.com>
2020-10-03 18:11:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -126,5 +126,17 @@ end
       self.new_from_db(row)
     end.first
   end
+  
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
 
