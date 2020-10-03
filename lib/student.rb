ab4983e7ccad3fdd47225baa3112610bf8d6d10e
Sara A <calisaraalhindi@gmail.com>
2020-10-03 17:36:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,12 +54,13 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
+      LIMIT 1 
       SQL
       
  
     DB[:conn].execute(sql, name).map do |row| 
       self.new_from_db(row)
-  end
+  end.first
 end
   
     def self.all
