2f685c5d9ad0a69c963b23c7e4a5004256314751
Sara A <calisaraalhindi@gmail.com>
2020-10-03 17:21:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
       LIMIT 1
     SQL
  
-    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, name).map do |row| self.new_from_db(row)
   end.first
 end
   
