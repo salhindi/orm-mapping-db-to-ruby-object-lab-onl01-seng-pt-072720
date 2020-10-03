bcd41c379601d29905ccea7412021aa145c2f61c
Sara A <calisaraalhindi@gmail.com>
2020-10-03 17:41:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,9 +117,7 @@ end
   def self.first_student_in_grade_10 
     self.first_X_students_in_grade_10(0)
     
-    
-    DB[:conn].execute(sql, x).map do |row| 
-      self.new_from_db(row)
+ 
   end
 end
 
