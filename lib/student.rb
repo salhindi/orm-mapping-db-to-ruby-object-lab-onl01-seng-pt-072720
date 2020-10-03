9b587c33981ccf273c205c2cfbc3202138624a66
Sara A <calisaraalhindi@gmail.com>
2020-10-03 17:51:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -115,8 +115,11 @@ end
 
   
   def self.first_student_in_grade_10 
-    
-    self.first_X_students_in_grade_10(0)
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade = '10'
+    LIMIT 1
     
     DB[:conn].execute
     
